39c70f3c0e592e3d0c6fd9d0fdb90744e5e6baea
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-04-10 11:26:54 +0530

    created category taking in functionality

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::io::*;
 
 
 fn main() {
-    println!("Hello, world!");
+    greet();
+    let x = category_taker();
 }
 
 fn greet() {
@@ -11,4 +12,34 @@ fn greet() {
     println!("https://github.com/lordofwizard");
     println!("for more information about this project ⬇");
     println!("https://github.com/lordofwizard/unit.git");
+}
+
+fn category_taker() -> category{
+    println!("Please input a category you want to do conversions on ");
+    println!("1. Temprature\n2. Mass");
+
+
+    // Taking u8 input
+    
+    let mut msg : String = String::new();
+    std::io::stdin().read_line(&mut msg).expect("Given input is not actually taken.. Some error occured");
+    let input : u8 = match msg.trim().parse(){
+        Ok(u) => u,
+        Err(x) => 0
+    };
+    if input == 1 {
+        category::Temprature
+    }
+    else if input == 2 {
+        category::Mass
+    }
+    else {
+        category::No
+    }
+
+}
+enum category{
+    Temprature,
+    Mass,
+    No
 }
