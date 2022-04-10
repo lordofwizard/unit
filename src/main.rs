0d85e3502d013c2fb83731e81d329ca3c289ad78
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-04-10 11:14:08 +0530

    wrote greet function

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::io::*;
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn greet() {
+    println!("Welcome to the unit conversion program created by LordOfWizard");
+    println!("follow on github.com ");
+    println!("https://github.com/lordofwizard");
+    println!("for more information about this project ⬇");
+    println!("https://github.com/lordofwizard/unit.git");
+}
